0af107ae265a9f1226c6607da6c28a60dfceb9eb
st-hieucao <hieu.cao@supremetech.vn>
2025-02-24 14:22:13 +0700

    download video success

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import HLSDownloader from './lib/HLSDownloader.js';
 
 (async () => {
-  const downloader = new HLSDownloader({ url: "https://vod.ottclouds.com/vods/9999/CcRxJOQgiwAFdpmYc7oezZM4Tvah7Lmp4/playlist.m3u8" });
+  const url = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+  const url2 = "https://vod.ottclouds.com/vods/9999/CcRxJOQgiwAFdpmYc7oezZM4Tvah7Lmp4/playlist.m3u8"
+  const downloader = new HLSDownloader({ url: url, idVideoIDB: 1 });
   console.log({ downloader })
   downloader.start();
 })();
