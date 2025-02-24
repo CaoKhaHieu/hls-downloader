5a908f57b39b05dea3baa667ed2b4281e8b169d9
st-hieucao <hieu.cao@supremetech.vn>
2025-02-24 11:14:41 +0700

    handle download start

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
@@ -7,4 +8,20 @@ module.exports = {
     filename: "hls-downloader.js",
   },
   mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html", // Tạo HTML từ template này
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  devtool: "source-map",
 };
