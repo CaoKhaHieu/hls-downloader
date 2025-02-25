a56f889a7c15aceed385080cbbc462836a9a048e
st-hieucao <hieu.cao@supremetech.vn>
2025-02-25 14:54:08 +0700

    example demo

--- index.js
+++ index.js
@@ -17,13 +17,79 @@ const mergeListArrayBuffer = (myArrays) => {
 };
 
 (async () => {
-  const url = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-  const url2 = "https://vod.ottclouds.com/vods/9999/CcRxJOQgiwAFdpmYc7oezZM4Tvah7Lmp4/playlist.m3u8"
-  const downloader = new HLSDownloader({ url: url2, idVideoIDB: 1 });
+  const url1 = "https://vtvgo-vods.vtvdigital.vn/RTJC-PMXuacVrExErCl_Tw/1740628543/vod/20250222/nong-trua-22t2.mp4/index.m3u8"
+  const url2 = "https://vtvgo-vods.vtvdigital.vn/of3wkYPedOwjEp6hK_77Sg/1740628756/vod/20250222/cd.mp4/index.m3u8"
+  const url3 = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+  const url4 = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
 
-  // start download
+  const downloader = new HLSDownloader({
+    url: url1,
+    idVideoIDB: 1,
+    thumbnail: 'https://picsum.photos/536/354',
+    subtitle: 'https://picsum.photos/536/354',
+    metadata: {
+      title: 'The Lorem Ipsum for photos.',
+      description: 'The Lorem Ipsum for photos.',
+      category: 'The Lorem Ipsum for photos.',
+      tags: ['video', 'hls', 'download'],
+    },
+    onSuccess: (data) => {
+      console.log('onSuccess', data);
+    },
+    onProgress: (data) => {
+      console.log('onProgress', data);
+    },
+    onPause: (data) => {
+      console.log('onPause', data);
+    },
+    onResume: (data) => {
+      console.log('onResume', data);
+    },
+    onCancel: (data) => {
+      console.log('onCancel', data);
+    },
+    onDelete: (data) => {
+      console.log('onDelete', data);
+    },
+    onDeleteAll: () => {
+      console.log('onDeleteAll');
+    }
+  });
+  const downloader2 = new HLSDownloader({
+    url: url2,
+    idVideoIDB: 2,
+    thumbnail: 'https://picsum.photos/536/354',
+    subtitle: 'https://picsum.photos/536/354',
+    metadata: {
+      title: 'The Lorem Ipsum for photos.',
+      description: 'The Lorem Ipsum for photos.',
+      category: 'The Lorem Ipsum for photos.',
+      tags: ['video', 'hls', 'download'],
+    },
+    onSuccess: (data) => {
+      console.log('onSuccess', data);
+    },
+    onProgress: (data) => {
+      console.log('onProgress', data);
+    },
+    onPause: (data) => {
+      console.log('onPause', data);
+    },
+    onResume: (data) => {
+      console.log('onResume', data);
+    },
+    onCancel: (data) => {
+      console.log('onCancel', data);
+    },
+    onDelete: (data) => {
+      console.log('onDelete', data);
+    }
+  });
+
+  // add event listener to start download
   document.getElementById('start').addEventListener('click', () => {
     downloader.start();
+    downloader2.start();
   });
 
   document.getElementById('pause').addEventListener('click', () => {
@@ -38,12 +104,22 @@ const mergeListArrayBuffer = (myArrays) => {
     downloader.cancel();
   });
 
-  // get video
+  document.getElementById('delete').addEventListener('click', () => {
+    downloader.deleteVideo(1);
+  });
+
+  document.getElementById('delete-all').addEventListener('click', () => {
+    downloader.deleteAllVideos();
+  });
+
+  // get videos
   const video = await downloader.getVideo(1);
+  const videos = await downloader.getAllVideos();
   if (!video) {
     return;
   }
 
+  // play video offline mode
   const mediaSource = new MediaSource();
   const transmuxer = new muxjs.mp4.Transmuxer();
   const mime = 'video/mp4; codecs="mp4a.40.2,avc1.64001f"';
@@ -70,4 +146,9 @@ const mergeListArrayBuffer = (myArrays) => {
     transmuxer.push(new Uint8Array(data));
     transmuxer.flush();
   };
+
+  // test get thumbnail video downloaded and preview
+  const thumbnail = await downloader.getThumbnailVideoDownloaded(1);
+  const thumbnailElement = document.querySelector('.thumbnail');
+  thumbnailElement.src = thumbnail;
 })();
